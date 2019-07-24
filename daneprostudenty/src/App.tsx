dc230f09b3b401562f5fb31d351f34240a5ed339
filip.valchar <filip.valchar@ideastatica.com>
2019-07-24 15:21:19 +0200

    basic nav

--- daneprostudenty/src/App.tsx
+++ daneprostudenty/src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-const App: React.FC = () => {
+const App: React.SFC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="ui container">
+        <div className="nav">
+          <img alt="logo" src="https://upload.wikimedia.org/wikipedia/en/thumb/3/31/Alexander_dennis_logo.png/150px-Alexander_dennis_logo.png" />
+          <div className="nav-right">
+            <a className="active" href="https://seznam.cz">O projektu</a>
+            <a className="" href="https://seznam.cz">Quis</a>
+            <a className="" href="https://seznam.cz">Turpis</a>
+            {/* <a className="ui positive circular button center-button" href="https://seznam.cz">Vytvorit danove priznani</a> */}
+          </div>
+        </div>
+      </div>
+      <div className="ui container">
+        <h1>Lorem ipsum dolor, <br />consectetuer adipiscing elit.</h1>
+      </div>
     </div>
   );
 }
